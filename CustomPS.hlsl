4b9970f6a45a403001c01285ffc0e2840317dbb7
Mudit1959 <143623442+Mudit1959@users.noreply.github.com>
2025-10-13 14:38:45 -0400

    Sin & Cos waves solved!

--- CustomPS.hlsl
+++ CustomPS.hlsl
@@ -26,6 +26,8 @@ float random(float2 s)
     return frac(sin(dot(s, float2(12.9898, 78.233))) * 43758.5453123);
 }
 
+static const float PI = 3.14159265f;
+
 // --------------------------------------------------------
 // The entry point (main method) for our pixel shader
 // 
@@ -41,14 +43,24 @@ float4 main(VertexToPixel input) : SV_TARGET
 	// - This color (like most values passing through the rasterizer) is 
 	//   interpolated for each pixel between the corresponding vertices 
 	//   of the triangle we're rendering
-    float sinVal = sin(input.uv.r * 25 * totalTime * 0.1);
-    float yVal = input.uv.g;
 	
-	// u goes from 0 - 25 if multiplied is x
-	// v goes from 0 - 25 if multiplied is y
-	// if v matches sin(u), I want to display it in red
-	// 
-    return float4(sinVal, 0.0f, 0.0f, 1.0f);
+	// u - r | v - g
+	// UV values range from 0-1
+	// To scale the x-axis to 25 - multiply u by 25 - new limits are 0-25
+	// To map the v values from -1 to 1 - multiply v by 2 then subtract 1 - new limits are -1-1
+	
+	
+    float sinVal = sin(input.uv.r*totalTime); // the multiplying factor with r controls the frequency/wavelength in an inverse manner
+    float cosVal = cos((input.uv.r*totalTime - PI)  * 10);
+    float yVal = -1 * ((input.uv.g * 6) - 3); 
+	// the multiplying factor with g controls the amplitude in an inverse manner | the subtracted value controls whether the wave is centered around the sphere
+	
+    float sinResultVal = 1 - (abs(yVal - sinVal)*8); 
+    float cosResultVal = 1 - (abs(yVal - cosVal) * 6);
+	// abs() ensures both the positive y and negative y axes are taken into account | 
+	// the multiplying factor controls the sharpness of the wave 
+	
+    return float4(sinResultVal, cosResultVal, 0.0f, 1.0f);
 
 }
 
